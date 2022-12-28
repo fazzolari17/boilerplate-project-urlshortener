5a72ba812312c5062aca4de68f006d9367725de9
fazzolari17 <42549543+fazzolari17@users.noreply.github.com>
2022-12-27 22:34:54 -0500

    initial commit

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const data = require('./data');
+// const bodyParser = require('body-parser');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,7 +22,46 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+  console.log(data)
 });
+
+app.post('/api/shorturl', (req, res) => {
+  console.log(req.body)
+  const { url } = req.body;
+
+
+  const httpRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+  
+  const goodUrl = httpRegex.test(url);
+  console.log(goodUrl)
+  if (!goodUrl) {
+    return res.json({
+      "error": "invalid url"
+    })
+  } else if (goodUrl) {
+    const shortenedUrl = {
+      "original_url": url,
+      "short_url": (data.length + 1).toString(),
+    };
+  
+    data.push(shortenedUrl);
+    return res.json(shortenedUrl);
+  }
+
+  
+});
+
+app.get('/api/shorturl/:id?', (req, res) => {
+  const id = req.params.id;
+  const found = data.find(item => item.short_url === id);
+
+
+  if (found) {
+    res.redirect(found.original_url)
+  }
+
+})
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
